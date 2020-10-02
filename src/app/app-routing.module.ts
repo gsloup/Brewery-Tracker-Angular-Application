c2839b4716bd1785ede1439b9407a4c88753380d
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-02 16:46:34 -0500

    adds routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'search', component: SearchComponent},
+  {path:'user/:username', component: UserComponent},
+  {path:'login', component: LoginComponent},
+  {path:'signup', component: SignupComponent},
+  {path:'**', redirectTo:'/login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
