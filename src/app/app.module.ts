ce062edbcace5537ae39f6eb0901fa91c8cb7651
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-02 16:28:38 -0500

    adds files for general app skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    UserComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
