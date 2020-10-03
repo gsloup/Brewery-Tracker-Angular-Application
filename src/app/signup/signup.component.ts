7af342b596cedb005d1a260bb2b942b6bbed5b89
Garrett Sloup <garrettsloup@gmail.com>
2020-10-03 14:46:44 -0500

    Adds working sign up component

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+
+  constructor(private fb:FormBuilder) { }
+
+  signup() {
+    if (this.signupForm.valid) {
+      // ADD CODE TO SIGN USER UP
+
+    }
+  }
 
   ngOnInit(): void {
+    // Builds the form here
+    this.signupForm = this.fb.group({
+      username: ['', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(20)])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(16)])]
+
+    })
   }
 
 }
