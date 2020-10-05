216fe7cbc0de3477d3980a9c8e7d9817d7aaf1c0
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-05 13:28:01 -0500

    adds uppercase pipe on table heading

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Brewery } from '../interfaces/brewery.interface';
 import { BreweryService } from '../services/brewery.service';
 import {animate, state, style, transition, trigger} from '@angular/animations';
-import { PhoneNumberPipe } from '../pipes/phone-number.pipe'
 
 @Component({
   selector: 'app-search',
@@ -36,7 +35,7 @@ export class SearchComponent implements OnInit {
   // dataSource = ELEMENT_DATA; // OG
   dataSource = this.breweries;
   // columnsToDisplay = ['name', 'weight', 'symbol', 'position']; // OG
-  columnsToDisplay = ['name', 'city', 'state']; // change to this for main headings
+  columnsToDisplay = ['name', 'city', 'state', 'favorite']; // change to this for main headings
   // expandedElement: PeriodicElement | null; // OG
   expandedElement: Brewery | null;
 
