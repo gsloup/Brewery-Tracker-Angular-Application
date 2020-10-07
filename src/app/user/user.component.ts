c71fc00d716527cc51ffa47fc8ecd144ec879892
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-07 10:55:03 -0500

    adds ngFor on cards to show favoritesList

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,16 +12,16 @@ export class UserComponent implements OnInit {
 
   favoritesList: Array<Brewery> = [];
   user: string;
-  breweryType: string = "";
-  breweryName: string = "";
-  websiteURL: string = "";
-  street: string = '';
-  city: string = '';
-  state: string = '';
-  zipCode: string = '';
-  phone: string = '';
-  latitude: string = '';
-  longitude: string = '';
+  // breweryType: string = "";
+  // breweryName: string = "";
+  // websiteURL: string = "";
+  // street: string = '';
+  // city: string = '';
+  // state: string = '';
+  // zipCode: string = '';
+  // phone: string = '';
+  // latitude: string = '';
+  // longitude: string = '';
 
   constructor(private favoritesService: FavoritesService, private userService: UserService) { }
 
@@ -30,17 +30,19 @@ export class UserComponent implements OnInit {
     this.favoritesService.favoritesList$.subscribe((res)=> {
       console.log(`this is the favoritesService ${res}`);
 
-    this.favoritesList = res;
-    this.breweryType = this.favoritesList[0].brewery_type; // FIX INDEX POSITION
-    this.breweryName = this.favoritesList[0].name;
-    this.websiteURL = this.favoritesList[0].website_url;
-    this.street = this.favoritesList[0].street;  
-    this.city = this.favoritesList[0].city;
-    this.state = this.favoritesList[0].state;
-    this.zipCode = this.favoritesList[0].postal_code;
-    this.phone = this.favoritesList[0].phone;
-    this.latitude = this.favoritesList[0].phone;
-    this.longitude = this.favoritesList[0].longitude;
+     this.favoritesList = res;
+
+     
+    // this.breweryType = this.favoritesList[0].brewery_type; // FIX INDEX POSITION
+    // this.breweryName = this.favoritesList[0].name;
+    // this.websiteURL = this.favoritesList[0].website_url;
+    // this.street = this.favoritesList[0].street;  
+    // this.city = this.favoritesList[0].city;
+    // this.state = this.favoritesList[0].state;
+    // this.zipCode = this.favoritesList[0].postal_code;
+    // this.phone = this.favoritesList[0].phone;
+    // this.latitude = this.favoritesList[0].phone;
+    // this.longitude = this.favoritesList[0].longitude;
 
     })
 
