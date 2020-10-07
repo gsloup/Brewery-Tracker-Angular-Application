d097cef0a71f5868eb8ee249d8d6692e6de267be
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-07 14:14:36 -0500

    adds icons to favorite breweries via asset folder

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FavoritesService } from '../services/favorites.service';
 import { Brewery } from '../interfaces/brewery.interface';
 import { UserService } from '../services/user.service';
+import { isDataSource } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-user',
@@ -12,6 +13,9 @@ export class UserComponent implements OnInit {
 
   favoritesList: Array<Brewery> = [];
   user: string;
+  iconFilenames = ['001-beer-1', '002-beer-bottle', '003-beer-box', '004-wheat', '005-beer-can', 
+      '006-bottle-cap', '007-beer', '008-barrel', '009-bar', '010-corkscrew'];
+  
 
   constructor(private favoritesService: FavoritesService, private userService: UserService) { }
 
@@ -28,5 +32,15 @@ export class UserComponent implements OnInit {
       this.user = res;
     })
   }
+
+  getIconFilename(idx: number): string { // Used to add brew icons from assets folder
+    let iconIndex: number = idx;
+    if (idx > 9) { // grabs the last index of a number > 9 and uses that value
+      iconIndex = parseInt(idx.toString().split("").reverse()[0]);
+    }
+    return (`../../assets/brewIcons/${this.iconFilenames[iconIndex]}.svg`);
+  }
+
+  
 
 }
