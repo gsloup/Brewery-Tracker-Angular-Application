cf088b31dfaa1394a010add087222d178c824a7a
Garrett Sloup <garrettsloup@gmail.com>
2020-10-09 15:39:06 -0500

    adds the UserGuard to specific route paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { SearchComponent } from './search/search.component';
 import { UserComponent } from './user/user.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { UserGuard } from './guards/user.guard';
 
 const routes: Routes = [
-  {path:'search', component: SearchComponent},
-  {path:'user/:username', component: UserComponent},
+  {path:'search', component: SearchComponent, canActivate: [UserGuard]},
+  {path:'user/:username', component: UserComponent, canActivate: [UserGuard]},
   {path:'login', component: LoginComponent},
   {path:'signup', component: SignupComponent},
   {path:'**', redirectTo:'/login'}
