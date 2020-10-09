fd2dde05a8ce7ace6d1e6d214f688ff201026d73
Garrett Sloup <garrettsloup@gmail.com>
2020-10-09 15:34:45 -0500

    adds conditionals on specific page routes

--- src/app/guards/user.guard.ts
+++ src/app/guards/user.guard.ts
@@ -1,15 +1,37 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserGuard implements CanActivate {
+  constructor(private userService: UserService, private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    
+      const username = this.userService.user$; // grabs the current 'user' that is logged in
+
+      // If no one is logged in, route them to the 'login' page
+      if (username === null) {  
+        this.router.navigate(['/login']);
+        return false;
+      }
+
+      // Allows a signed in person to access the home/search page of the app
+      if (state.url === "/search") {
+        return true;
+      }
+
+      // Makes sure whoever is logged in can only go to their user page
+      if (username.toString().toLowerCase() != route.params.username.toLowerCase()) {
+        this.router.navigate([`user/${username}`]);
+      }
+
+      return true;
   }
   
 }
