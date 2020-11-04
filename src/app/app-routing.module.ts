4b0c0b7dadc882c1ba1ec5fe581e484e9c6002a2
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-11-04 15:14:29 -0600

    adds generic guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { UserComponent } from './user/user.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { UserGuard } from './guards/user.guard';
+import { GenericGuard } from './guards/generic.guard';
 
 const routes: Routes = [
   {path:'search', component: SearchComponent, canActivate: [UserGuard]},
   {path:'user/:username', component: UserComponent, canActivate: [UserGuard]},
-  {path:'login', component: LoginComponent},
-  {path:'signup', component: SignupComponent},
+  {path:'login', component: LoginComponent, canActivate: [GenericGuard]},
+  {path:'signup', component: SignupComponent, canActivate: [GenericGuard]},
   {path:'**', redirectTo:'/login'}
 ];
 
--- src/app/guards/generic.guard.ts
+++ src/app/guards/generic.guard.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenericGuard implements CanActivate {
+
+  constructor(private userService: UserService, private router: Router) {
+
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot): Observable<boolean> {
+      return this.userService.user$.pipe(
+        map((username: string) => {
+          // route logged in users back to search page
+          if (username !== null) {
+            this.router.navigate(['/search']);
+            return false;
+          }
+          // otherwise allow non-logged in users to access the page
+          return true;
+        })
+      )
   }
   
 }
